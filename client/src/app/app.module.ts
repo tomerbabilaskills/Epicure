7a382e029568a941080e77d142241360e27aa373
Tomer Babila <tomerb@academy-skills.com>
2021-07-11 09:39:35 +0300

    Add footer

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ItemComponent } from './components/item/item.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, HeaderComponent, ItemComponent],
+  declarations: [AppComponent, HomePageComponent, HeaderComponent, ItemComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
