40a349a44fc222521359ca33bef34c09da1fe3a7
Tomer Babila <tomerb@academy-skills.com>
2021-07-13 12:24:49 +0300

    Add flex-layout library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,7 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
     AboutComponent,
     SearchBarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
